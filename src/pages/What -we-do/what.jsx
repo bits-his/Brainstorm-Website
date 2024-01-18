b69f6617ecf38a69be4864846d91d66016f8f3d6
69rc <jibrinb271@gmail.com>
2024-01-18 10:38:32 +0100

    self

---
+++ src/pages/What -we-do/what.jsx
@@ -0,0 +1,28 @@
+import './what.css'
+import '../../'
+
+function what() {
+  return (
+   <>
+   <div className="container bg-light">
+    <div className="what-text">
+        <h2>Every Part of Your IT Ecosystem. Taken Care Of.</h2>
+        <div className='dot'></div>
+        <p>Comprehensive care of your cloud or on-premises infrastructure and applications:</p>
+        <ul>
+            <li>consulting</li>
+            <li>implementation</li>
+            <li>maganagement and support</li>
+            <li>security</li>
+            <li>help desk and services</li>
+            <li>migration</li>
+        </ul>
+        <div className="btn">explore</div>
+    </div>
+    <div className="what-img"></div>
+   </div>
+   </>
+  )
+}
+
+export default what
