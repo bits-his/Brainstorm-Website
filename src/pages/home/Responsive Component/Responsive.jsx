7d4136e34bdbe731de95e77942f7096d96d718e3
uniboika <nazifabdullahi003@gmail.com>
2024-01-19 12:40:10 +0100

    save changes

---
+++ src/pages/home/Responsive Component/Responsive.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import Trends from "./tabs/Trends";
+import Soft from "./software/Soft";
+
+export default function Responsive() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <div className="responsive-component">
+      {windowWidth <= 1500 && windowWidth >= 768 && (
+        <Trends />
+      )}
+      {windowWidth < 768 && (
+        <Soft />
+      )}
+    </div>
+  );
+};
+
