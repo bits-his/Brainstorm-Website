a5f92b8b40312e76db89b5b5734175fccb863bdd
uniboika <nazifabdullahi003@gmail.com>
2024-01-19 12:16:22 +0100

    save changes

--- src/pages/Bi-hub/CustumeCard.jsx
+++ src/pages/Bi-hub/CustumeCard.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import "./custumcard.css";
 
-export default function CustumeCard({ image = {}, alt = "", title = "", text = "", col = "", col2 = ""}) {
+export default function CustumeCard({
+  image = {},
+  alt = "",
+  title = "",
+  text = "",
+  col = "",
+  col2 = "",
+  number = "",
+}) {
   return (
     <>
       <div className={`col-lg-${col} col-md-${col2}`}>
         <div class="carded">
-          <img src={image} alt={alt} />
+          <div className="d-block">
+            <img src={image} alt={alt} style={{ width: `${number}px` }} />
+            <span className="d-lg-none d-block text-center mt-3 ">
+              Click Me
+            </span>
+          </div>
           <div class="carded__content">
             <p class="carded__title">{title}</p>
             <p class="carded__description">{text}</p>
--- src/pages/home/software/Soft.jsx
+++ src/pages/home/software/Soft.jsx
@@ -1,123 +1,94 @@
 import React from "react";
-import { useState } from "react";
 import "./soft.css";
-import Consulting from "./Consulting";
-import Apllication from "./Apllication";
-import Ui from "./Ui";
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
 
 export default function Soft() {
-  const [activeTab, setActiveTab] = useState("home");
-
-  const handleTabClick = (tab) => {
-    setActiveTab(tab);
-  };
-
+const options = {
+  items: 1,
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 4000,
+  animateOut: "slideOutUp",
+  nav: false,
+  margin: 0,
+ 
+};
   return (
     <>
-      <div className="software">
-
-        <div className="software-content">
-        <h1>Discover Our Range</h1>
-          <nav className="tab">
-            <ul>
-              <li onClick={() => handleTabClick("home")}>Home</li>
-              <li onClick={() => handleTabClick("about")}>About</li>
-              <li onClick={() => handleTabClick("contact")}>Contact</li>
-            </ul>
-          </nav>
-
-          <div className="tab-content">
-            {activeTab === "home" && <Consulting />}
-            {activeTab === "about" && <Apllication />}
-            {activeTab === "contact" && <Ui />}
+      <OwlCarousel className="owl-theme" {...options}>
+        <div class="item" style={{ width: 100 }}>
+          <div className="caro">
+              <h1>hggs</h1>
           </div>
         </div>
-      </div>
-
-      {/* <div className="accordion accordion-flush" id="accordionFlushExample">
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="flush-headingOne">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#flush-collapseOne"
-              aria-expanded="false"
-              aria-controls="flush-collapseOne"
-            >
-              Accordion Item #1
-            </button>
-          </h2>
-          <div
-            id="flush-collapseOne"
-            className="accordion-collapse collapse show"
-            aria-labelledby="flush-headingOne"
-            data-bs-parent="#accordion"
-          >
-            <div className="accordion-body">
-              Placeholder content for this accordion, which is intended to
-              demonstrate the <code>.accordion-flush</code> className. This is
-              the first item's accordion body.
-            </div>
+        <div class="item" style={{ width: 100 }}>
+          <div className="caro">
+              <h1>hggshfhfh</h1>
           </div>
         </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="flush-headingTwo">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#flush-collapseTwo"
-              aria-expanded="false"
-              aria-controls="flush-collapseTwo"
-            >
-              Accordion Item #2
-            </button>
-          </h2>
-          <div
-            id="flush-collapseTwo"
-            className="accordion-collapse collapse"
-            aria-labelledby="flush-headingTwo"
-            data-bs-parent="#accordion"
-          >
-            <div className="accordion-body">
-              Placeholder content for this accordion, which is intended to
-              demonstrate the <code>.accordion-flush</code> className. This is
-              the second item's accordion body. Let's imagine this being filled
-              with some actual content.
-            </div>
+        <div class="item" style={{ width: 100 }}>
+          <div className="caro">
+              <h1>hggshfhfh</h1>
           </div>
         </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="flush-headingThree">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#flush-collapseThree"
-              aria-expanded="false"
-              aria-controls="flush-collapseThree"
-            >
-              Accordion Item #3
-            </button>
-          </h2>
-          <div
-            id="flush-collapseThree"
-            className="accordion-collapse collapse"
-            aria-labelledby="flush-headingThree"
-            data-bs-parent="#accordion"
-          >
-            <div className="accordion-body">
-              Placeholder content for this accordion, which is intended to
-              demonstrate the <code>.accordion-flush</code> className. This is
-              the third item's accordion body. Nothing more exciting happening
-              here in terms of content, but just filling up the space to make it
-              look, at least at first glance, a bit more representative of how
-              this would look in a real-world application.
-            </div>
+        <div class="item" style={{ width: 100 }}>
+          <div className="caro">
+              <h1>hggshfhfh</h1>
           </div>
         </div>
-      </div> */}
+        <div class="item" style={{ width: 100 }}>
+          <div className="caro">
+              <h1>hggsff</h1>
+          </div>
+        </div>
+        <div class="item" style={{ width: 100 }}>
+          <div className="caro">
+              <h1>hggs</h1>
+          </div>
+        </div>
+        <div class="item" style={{ width: 100 }}>
+          <div className="caro">
+              <h1>hggs</h1>
+          </div>
+        </div>
+        <div class="item" style={{ width: 100 }}>
+          <div className="caro">
+              <h1>hggs</h1>
+          </div>
+        </div>
+        <div class="item" style={{ width: 100 }}>
+          <div className="caro">
+              <h1>hggs</h1>
+          </div>
+        </div>
+        <div class="item" style={{ width: 100 }}>
+          <div className="caro">
+              <h1>hggs</h1>
+          </div>
+        </div>
+        <div class="item" style={{ width: 100 }}>
+          <div className="caro">
+              <h1>hggs</h1>
+          </div>
+        </div>
+        <div class="item" style={{ width: 100 }}>
+          <div className="caro">
+              <h1>hggs</h1>
+          </div>
+        </div>
+        <div class="item" style={{ width: 100 }}>
+          <div className="caro">
+              <h1>hggs</h1>
+          </div>
+        </div>
+        <div class="item" style={{ width: 100 }}>
+          <div className="caro">
+              <h1>hggs</h1>
+          </div>
+        </div>
+      </OwlCarousel>
     </>
   );
 }
