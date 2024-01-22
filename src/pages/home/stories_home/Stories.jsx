4f72889d16acf1383b2c35790fffa1eacc3ad582
Phisherman <117809934+Phisherman-0@users.noreply.github.com>
2024-01-22 10:40:06 +0100

    Class changed to className

--- src/pages/home/stories_home/Stories.jsx
+++ src/pages/home/stories_home/Stories.jsx
@@ -10,8 +10,8 @@ function Stories() {
               <p className='underline shift'></p>
             </div>
             <div className='row'>
-              <div class="flex-container">
-                <div class="item1">
+              <div className="flex-container">
+                <div className="item1">
                   <div className='headline-stories'>
                     <div className='stories-head'>Software Products Development for Global Webpages</div>
                     <p className='underline'></p>
@@ -32,7 +32,7 @@ function Stories() {
                   </div>
 
                 </div>
-                <div class="item2">
+                <div className="item2">
                   <div className='headline-stories'>
                     <div className='stories-head'>Creating and Securing Dynamic Online Platforms (Web 3)</div>
                     <p className='underline'></p>
@@ -50,7 +50,7 @@ function Stories() {
                     </button>
                   </div>
                 </div>
-                <div class="item3">
+                <div className="item3">
                   <div className='headline-stories'>
                     <div className='stories-head'> Managing Robust Backends</div>
                     <p className='underline'></p>
@@ -69,7 +69,7 @@ function Stories() {
                     </button>
                   </div>  
                 </div>  
-                <div class="item4">
+                <div className="item4">
                   <div className='headline-stories'>
                     <div className='stories-head'>Providing Hosting Services</div>
                     <p className='underline'></p>
@@ -87,7 +87,7 @@ function Stories() {
                     </button>
                   </div>
                 </div>
-                <div class="item5">
+                <div className="item5">
                   <div className='headline-stories'>
                     <div className='stories-head'>Utilizing Core Technologies for Efficient Servicing and Cybersecurity</div>
                     <p className='underline'></p>
