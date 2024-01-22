bd38007b351af4afe9f9d028c2664fdaad5c60df
Phisherman <117809934+Phisherman-0@users.noreply.github.com>
2024-01-22 16:58:53 +0100

    flows

--- src/pages/home/brief-contact-us/GetInTouch.jsx
+++ src/pages/home/brief-contact-us/GetInTouch.jsx
@@ -10,7 +10,7 @@ export default function getInTouch(){
                             <div className="content">
                                 <h6 className='shout-out'>GIVE US SHOUT</h6>
                                 <h1 className="mt-3">Are you Ready to have a Talk?</h1>
-                                <button className="btn btn-primary rounded-pill cont">Contact Us</button>
+                                <button className="btn btn-primary rounded-pill cont">Contact us</button>
                             </div>
                         </div>
                     </div>
