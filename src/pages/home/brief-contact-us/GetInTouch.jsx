0289acd88df6f98592f5416c16d1c99b4fd0c639
Phisherman <117809934+Phisherman-0@users.noreply.github.com>
2024-01-22 16:52:20 +0100

    Get in touch updated, please change the image

--- src/pages/home/brief-contact-us/GetInTouch.jsx
+++ src/pages/home/brief-contact-us/GetInTouch.jsx
@@ -8,9 +8,9 @@ export default function getInTouch(){
                     <div className="row">
                         <div className="fixed-background">
                             <div className="content">
-                                <h6>GIVE US SHOUT</h6>
+                                <h6 className='shout-out'>GIVE US SHOUT</h6>
                                 <h1 className="mt-3">Are you Ready to have a Talk?</h1>
-                                <button className="btn btn-primary rounded-pill">Contact Us</button>
+                                <button className="btn btn-primary rounded-pill cont">Contact Us</button>
                             </div>
                         </div>
                     </div>
