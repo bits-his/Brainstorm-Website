36538b66220b81aad1f3c376070ebbc0f2106cbf
Phisherman <117809934+Phisherman-0@users.noreply.github.com>
2024-01-22 12:40:50 +0100

    removed blur

--- src/pages/home/stories_home/Stories.jsx
+++ src/pages/home/stories_home/Stories.jsx
@@ -112,6 +112,7 @@ function Stories() {
           <div className="container-fluid">
             <div className="row">
               <div className="fixed-background">
+                
                 <div className="content">
                   <p>This is the content that will scroll over the fixed background.</p>
                 </div>
