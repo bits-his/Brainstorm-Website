fe30c985edc7fcb3be091472aff47d4154b675fb
Nazif Abdullahi <nazifabdullahi003@gmail.com>
2024-02-08 11:48:24 +0100

    save

--- src/pages/home/What -we-do/what.jsx
+++ src/pages/home/What -we-do/what.jsx
@@ -8,90 +8,90 @@ import { BiCheckDouble } from "react-icons/bi";
 function what() {
   return (
     <>
-    <div className="bg-light ">
-      <div className="container mt-5 d-flex p-4 mb-5 m-5 what ">
-      <div className="what-img m-3" >
-          <img src={Img} alt="" className="img-fluid" />
+      <div className="bg-light ">
+        <div className="container mt-5 d-flex p-4 mb-5 m-5 what ">
+          <div
+            className="what-img m-3"
+            data-aos="zoom-in-left"
+            data-aos-once="true"
+          >
+            <img src={Img} alt="" className="img-fluid" />
+          </div>
+          <div className="what-text p-3">
+            <h3 data-aos="fade-right" data-aos-once="true">
+              Make your business more competitive and <br />
+              sustainable.
+            </h3>
+
+            <div data-aos="fade-up" data-aos-once="true">
+              <span className="span">
+                Holistic Management for Your Cloud or On-Premises Infrastructure
+                and Applications.
+              </span>
+              <ul>
+                <li>
+                  <BiCheckDouble className="icon" />
+                  Influencing Sales
+                </li>
+                <li>
+                  <BiCheckDouble className="icon" />
+                  Generating Insights
+                </li>
+                <li>
+                  <BiCheckDouble className="icon" />
+                  Boosting Operations
+                </li>
+                <li>
+                  <BiCheckDouble className="icon" />
+                  security
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-        <div className="what-text p-3">
-          <h3>
-          Make your business more competitive and  <br />sustainable.
-          </h3>
-          
-          <span className="span">
-            Holistic Management for Your Cloud or On-Premises Infrastructure and
-            Applications.
-          </span>
-          <ul>
-            <li>
-              <BiCheckDouble className="icon" />
-              
-Influencing Sales
-            </li>
-            <li>
-              <BiCheckDouble className="icon" />
-              Generating Insights
-            </li>
-            <li>
-              <BiCheckDouble className="icon" />
-              Boosting Operations
-            </li>
-            <li>
-              <BiCheckDouble className="icon" />
-              security
-            </li>
-            
-          </ul>
-         
-        </div>
-        
-      </div>
       </div>
       <div className="container mt-5 d-flex p-4 mb-5 m-5 what">
-      
         <div className="what-text p-3">
-          <h3>
-          Make your business more competitive and  <br />sustainable.
+          <h3 data-aos="fade-right" data-aos-once="true">
+            Make your business more competitive and <br />
+            sustainable.
           </h3>
-          
-          <span className="span">
-            Holistic Management for Your Cloud or On-Premises Infrastructure and
-            Applications.
-          </span>
-          <ul>
-            <li>
-              <BiCheckDouble className="icon" />
-              consulting
-            </li>
-            <li>
-              <BiCheckDouble className="icon" />
-              implementation
-            </li>
-            <li>
-              <BiCheckDouble className="icon" />
-              Engaging Customer
-            </li>
-            <li>
-              <BiCheckDouble className="icon" />
-              security
-            </li>
-            
-          </ul>
-         
+
+          <div data-aos="fade-up" data-aos-once="true">
+            <span className="span">
+              Holistic Management for Your Cloud or On-Premises Infrastructure
+              and Applications.
+            </span>
+            <ul>
+              <li>
+                <BiCheckDouble className="icon" />
+                consulting
+              </li>
+              <li>
+                <BiCheckDouble className="icon" />
+                implementation
+              </li>
+              <li>
+                <BiCheckDouble className="icon" />
+                Engaging Customer
+              </li>
+              <li>
+                <BiCheckDouble className="icon" />
+                security
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="what-img m-3" >
+        <div
+          className="what-img m-3"
+          data-aos="zoom-in-left"
+          data-aos-once="true"
+        >
           <img src={Img2} alt="" className="img-fluid" />
         </div>
-        
       </div>
-  
     </>
   );
 }
 
 export default what;
-
-
-
-
-
