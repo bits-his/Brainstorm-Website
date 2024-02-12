5a011ac07bd7617a28e76f84e56cdde3dc1ab406
Nazif Abdullahi <nazifabdullahi003@gmail.com>
2024-02-12 13:19:40 +0100

    revert -ahmad

--- src/pages/about-us/AboutUs.jsx
+++ src/pages/about-us/AboutUs.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 import "./about-us.css";
 // import Hero from '../../Components/Hero/Hero'
-import Img from "../../assets/img/ensuring.jpg";
+import Img from "../../assets/download.jpg";
 import TeamContainer from "./TeamContainer";
 import Parallex from "../../Components/parallex/Parallex";
-import Designed from "../home/Design/Designed";
-import AboutCard from "./AboutCard";
-// import WD from '../../pages/services/img/WD.webp'
-// import CSD from '../../pages/services/img/CSD.webp'
-// import CT from '../../pages/services/img/CT.png'
-
 export default function AboutUs() {
   let startYear = 2006;
   let currentYear = new Date().getFullYear();
@@ -17,14 +11,11 @@ export default function AboutUs() {
 
   return (
     <>
-      <div className="about-head-img">
-        <div className="container">
-          <div>About Us</div>
-        </div>
-        <div className="strip blue"></div>
-        <div className="strip red"></div>
-      </div>
-      <div className="container mt-1 d-flex p-4 mb- about-container what">
+      <Parallex
+        page={"About Us"}
+        details
+      />
+      <div className="container mt-5 d-flex p-4 mb-5 about-container what">
         <div className="what-text ">
           <h2
             data-aos="fade-right"
@@ -33,10 +24,9 @@ export default function AboutUs() {
           >
             About Us
           </h2>
-          <div data-aos="fade-up" data-aos-once="true" className="about-text">
+          <div data-aos="fade-up" data-aos-once="true">
             <h1 className="spa">
-              We have been Innovating for over{" "}
-              <span className="years">{years}</span>
+              We have be Innovating for <span className="years">{years}</span>
             </h1>
             <p>
               Brainstorm IT Solutions is a software solution company with a wide
@@ -54,36 +44,14 @@ export default function AboutUs() {
           </div>
         </div>
         <div
-          className="col-md-5 about-img"
+          className="what-img m-3"
           data-aos="zoom-in-left"
           data-aos-once="true"
         >
-          <img src={Img} alt="aboutplaceholder" />
+          <img src={Img} alt="" className />
         </div>
       </div>
 
-      <div className="row service-container container">
-        <AboutCard
-          heading="Our Mission"
-          describtion="Enhance your online presence with cutting-edge web applications. We specialize in creating dynamic and user-friendly web solutions that deliver seamless experiences across devices."
-          // img={WD}
-          time={100}
-        />
-        <AboutCard
-          heading="Our History"
-          describtion="Empower your business with cutting-edge software solutions tailored to your unique needs. Our software development expertise ensures scalable and efficient solutions accross all devices."
-          // img={CSD}
-          time={200}
-        />
-        <AboutCard
-          heading="Who we are "
-          describtion="In addition to our development services, we offer corporate training programs designed to enhance the skills and capabilities of your team. From the latest technologies to industry best practices."
-          // img={CT}
-          time={300}
-        />
-      </div>
-
-      <Designed />
       {/* <Hero page='About'/> */}
 
       <TeamContainer />
